31a000751246017e2976bf21b141c549fd5dfbe7
Billy Salinas <wsalinas315@gmail.com>
2022-10-15 20:58:10 -0500

    updated console outputs and comments for testing

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -8,15 +8,20 @@ let basket = [];
 // Create function addItem to add item string to 
 function addItem(item){
     basket.push(item);
-    // Log item and updated basket to the console
-    console.log("Item", item, "has been added to your cart.");
+    // console.log("Item", item, "has been added to your cart.");
     return true;
 }
 
-//Test addItems function by adding a RTX 4090Ti, Spark Plug Wrench and Husky Puppy to the basket
-addItem('RTX 4090Ti');
-addItem('Spark Plug Wrench');
-addItem('Husky puppy');
+// Log empty basket contents to console
+console.log(`Contents of basket are: ${basket}`);
+
+// Test addItems function by adding a RTX 4090Ti, Spark Plug Wrench and Husky Puppy to the basket
+console.log('Adding RTX 4090Ti (expect true)', addItem('RTX 4090Ti'));
+console.log('Adding Spark Plug Wrench (expect true)', addItem('Spark Plug Wrench'));
+console.log('Adding Husky Puppy (expect true)', addItem('Husky Puppy'));
+
+// Console log updated basket contents
+console.log(`Contents of basket are now: ${basket}`);
 
 // Create function to list items in the basket
 function listItems(){
@@ -25,9 +30,11 @@ function listItems(){
         for(let item of basket){
             console.log(item);
         }
+        return true;
     } else{
-        console.log('Your cart is empty.');
-    }   
+        //console.log('Your cart is empty.');
+        return 'Your cart is empty.';
+    }
 }
 
 // Tests the listItems function and prints out basket contents
@@ -36,9 +43,20 @@ listItems();
 // Create function to empty the basket
 function empty(){
     basket = [];
-    console.log('Your cart has been emptied.');
+    //console.log('Your cart has been emptied.');
+    return true;
 }
 
-// Tests the empty function
-empty();
-listItems();
+// Run the empty function and test listItems for an empty basket
+console.log('Emptying cart (expect true)', empty());
+console.log(`Listing contents for empty cart (expect 'Your cart is empty')`, listItems());
+
+// STRETCH GOALS
+// define a maximum number of items of 5
+const maxItems = 5;
+
+// Create isFull function to test if the basket exceed maxItems
+function isFull(){
+
+}
+
